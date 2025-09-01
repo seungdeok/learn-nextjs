368d41677be1612ca33c73d89f64fbe8b3a8121d
seungdeok <akk9132@naver.com>
2025-09-01 22:27:31 +0900

    feat(section3): add styling & mock data

--- section03/src/components/search-bar.tsx
+++ section03/src/components/search-bar.tsx
@@ -1,26 +1,40 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import style from "./search-bar.module.css";
 
 export default function SearchBar() {
   const router = useRouter();
+  const searchParams = useSearchParams();
   const [search, setSearch] = useState("");
+  const q = searchParams.get("q");
 
   const handleChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
   const handleSubmit = () => {
+    if (!search || q === search) return;
     router.push(`/search?q=${search}`);
   };
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSubmit();
+    }
+  };
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
   return (
-    <div>
+    <div className={style.container}>
       <input
         value={search}
         onChange={handleChangeSearch}
-        placeholder="검색어를 입력하세요"
+        onKeyDown={handleKeyDown}
       />
       <button onClick={handleSubmit}>검색</button>
     </div>
