c7dcb2f077de577cd3c53cf171104ce98de0fe82
seungdeok <akk9132@naver.com>
2025-09-01 23:20:04 +0900

    feat(section3): add data fetching

--- section03/src/app/(with-searchbar)/page.tsx
+++ section03/src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,45 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
+import { BookData } from "@/types";
+
+async function Books() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/book`);
+  
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>
+  }
+  
+  const books: BookData[] = await response.json();
+
+  return books?.map((book) => (
+    <BookItem key={book.id} {...book} />
+  ))
+}
+
+async function RecommendBooks() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/book/random`);
+  
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>
+  }
+  
+  const books: BookData[] = await response.json();
+
+  return books?.map((book) => (
+    <BookItem key={book.id} {...book} />
+  ))
+}
 
 export default function Page() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecommendBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <Books />
       </section>
     </div>
   );
