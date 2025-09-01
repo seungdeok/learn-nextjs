ccc73d7fe903bf20d7f853bb65aeeeb00d4353e8
seungdeok <akk9132@naver.com>
2025-09-01 23:38:08 +0900

    feat(section3): add footer using request memorization

--- section03/src/app/layout.tsx
+++ section03/src/app/layout.tsx
@@ -2,11 +2,28 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { BookData } from "@/types";
 
 export const metadata: Metadata = {
   title: "ONEBITE BOOKS",
   description: "ONEBITE BOOKS",
 };
+
+async function Footer() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/book`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>
+  }
+  const books: BookData[] = await response.json();
+
+  return (
+    <footer>
+      제작 @seungdeok
+      <br />
+      {books.length}개의 도서가 있습니다.
+    </footer>
+  )
+}
 
 export default function RootLayout({
   children,
@@ -21,7 +38,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @seungdeok</footer>
+          <Footer />
         </div>
       </body>
     </html>
