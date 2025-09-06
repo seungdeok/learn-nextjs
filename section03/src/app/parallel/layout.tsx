e11e1c1b6be63b4ba33c2f2c6dbe21e1d8212a4f
seungdeok <akk9132@naver.com>
2025-09-06 14:46:45 +0900

    feat(section3): parallel route

---
+++ section03/src/app/parallel/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({ children, sidebar }: { children: React.ReactNode, sidebar: React.ReactNode }) {
+  return (
+    <div>
+      {sidebar}
+      {children}
+    </div>
+  );
+}
